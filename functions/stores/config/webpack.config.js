f3ba89ce071b0a21c601d2c6ec4036f037747fd5
AGL\A136836 <amarambe@agl.com.au>
2017-11-01 16:49:05 +1100

    Added the initial files.

---
+++ functions/stores/config/webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/storesHandler'
+    ],
+    watch: false,
+    target: 'node',
+    externals: [nodeExternals()],
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            use: 'babel-loader',
+            exclude: /node_modules/
+        }]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            'config/storesSAM.yaml'
+        ]),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.DefinePlugin({
+            "process.env": {
+                "BUILD_TARGET": JSON.stringify('server')
+            }
+        }),
+    ],
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, '../build'),
+        filename: 'storesHandler.js'
+    }
+}
