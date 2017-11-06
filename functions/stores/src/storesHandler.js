ca59055c1750e1340fa541649dcd6b3c1d01b764
Arjuna Marambe <amarambe@agl.com.au>
2017-11-06 16:03:12 +1100

    Did some small modifications.

--- functions/stores/src/storesHandler.js
+++ functions/stores/src/storesHandler.js
@@ -4,6 +4,9 @@ import { SampleStores } from './sampleData';
 module.exports.get = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+    },
     body: JSON.stringify({
       stores: SampleStores,
       input: event,
