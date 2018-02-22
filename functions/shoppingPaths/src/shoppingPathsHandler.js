6524d6e8bb731ea02d7994bbd72cf830ca7f79a1
Arjuna Marambe <amarambe@agl.com.au>
2018-02-23 07:11:13 +1100

    Added support for apollo-client.

--- functions/shoppingPaths/src/shoppingPathsHandler.js
+++ functions/shoppingPaths/src/shoppingPathsHandler.js
@@ -1,31 +1,50 @@
 import { runGraphQL } from './graphql/lib';
 
 const getShoppingPaths = (event, context, callback) => {
-    // runGraphQL(event, (error, result) => {
-    //     const response = {
-    //         statusCode: 200,
-    //         headers: {
-    //             "Access-Control-Allow-Origin": "*" // Required for CORS support to work
-    //         },
-    //         body: JSON.stringify({
-    //             ShoppingPaths: result.data.ShoppingPaths,
-    //             input: event,
-    //         }),
-    //     };
-    //     callback(error, response);
-    // });
+    runGraphQL(event, (error, result) => {
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+            },
+            body: JSON.stringify({
+                data: {
+                    ShoppingPaths: result.data.ShoppingPaths,
+                    input: event,
+                }
+            }),
+        };
+        callback(error, response);
+    });
 
+    // const response = {
+    //     statusCode: 200,
+    //     headers: {
+    //         'Access-Control-Allow-Origin': "*" // Required for CORS support to work,
+    //     },
+    //     body: JSON.stringify({
+    //         data: {
+    //             ShoppingPaths: 'result.data.ShoppingPaths',
+    //             input: event,
+    //         }
+    //     }),
+    // };
+    // callback(null, response);
+}
+
+const getOptions = (event, context, callback) => {
     const response = {
         statusCode: 200,
         headers: {
-            "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+            'Access-Control-Allow-Origin': "*", // Required for CORS support to work,
+            'Access-Control-Allow-Headers': 'content-type',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
         },
         body: JSON.stringify({
-            ShoppingPaths: 'result.data.ShoppingPaths',
-            input: event,
+            Options: 'Options'
         }),
     };
     callback(null, response);
 }
 
-export { getShoppingPaths };
+export { getShoppingPaths, getOptions };
